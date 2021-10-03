be2c39df8975a2327c7e95e837dfd4b137d1ac2e
anon-legion <g.sync.mobile1@gmail.com>
2021-10-03 11:43:29 +0800

    refactor App.js for hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import engineResources from './scripts/engine.js';
 const { computerPlay, playRound} = engineResources;
 
 function App() {
+  const [playerScore, setPlayerScore] = useState(() => 0);
+  const [compScore, setCompScore] = useState(() => 0);
+  const gameVals = ['rock', 'paper', 'scissors'];
+  const rock = <img src='./images/rps_rock.png' />;
+  const paper = <img src='./images/rps_paper.png' />;
+  const scisorrs = <img src='./images/rps_scissors.png' />;
+  let userInput, compInput, result;
+
+  const playerWin = () => {
+    setPlayerScore(currentScore => currentScore + 1);
+  };
+
+  const computerWin = () => {
+    setCompScore(currentScore => currentScore +1);
+  }
+
+  function btnOnClick(e) {
+    userInput = e.target.getAttribute('data-value');
+    compInput = computerPlay();
+    console.log(`computerInput =\t${compInput}`);
+    result = playRound(userInput, compInput);
+    switch (result) {
+      case 'win':
+        playerWin();
+        break;
+      case 'lose':
+        computerWin();
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,32 +55,18 @@ function App() {
           </div>
           <div className="computer">
             <h1>Comp</h1>
-            <h2>{computerScore}</h2>
+            <h2>{compScore}</h2>
           </div>
         </div>
         <div className="btn-group">
-          <button data-value="rock">Rock</button>
-          <button data-value="paper">Paper</button>
-          <button data-value="scissors">Scissors</button>
+          <button onClick={btnOnClick} data-value={gameVals[0]}>Rock</button>
+          <button onClick={btnOnClick} data-value={gameVals[1]}>Paper</button>
+          <button onClick={btnOnClick} data-value={gameVals[2]}>Scissors</button>
         </div>
       </div>
     </div>
   );
 }
-
-const btnAll = document.querySelectorAll('button');
-let userInput, computerInput, playerScore = 0, computerScore = 0;
-
-function btnOnClick(e) {
-  userInput = e.target.getAttribute('data-value');
-  computerInput = computerPlay();
-  console.log(`userInput =\t${userInput}\ncomputerInput =\t${computerInput}`);
-  console.log(playRound(userInput, computerInput));
-}
-
-btnAll.forEach(button => {
-  button.addEventListener('click', btnOnClick);
-});
 
 
 export default App;
