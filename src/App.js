9238c7fe761a4fc053673a07256c030b94583d2b
anon-legion <g.sync.mobile1@gmail.com>
2021-10-04 14:22:59 +0800

    update game hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useReducer} from 'react';
 import './App.css';
 import engineResources from './scripts/engine.js';
-const { computerPlay, playRound} = engineResources;
+const { computerPlay, playRound } = engineResources;
+
+const rock = <img src='./images/rps_rock.png' />;
+const paper = <img src='./images/rps_paper.png' />;
+const scissors = <img src='./images/rps_scissors.png' />;
+
+const ACTIONS = {
+  ROCK: 'rock',
+  PAPER: 'paper',
+  SCISSORS: 'scissors'
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.ROCK:
+      return { player: rock, computer: window[`${action.payload}`] };
+      break;
+    case ACTIONS.PAPER:
+      return { player: paper, computer: window[`${action.payload}`] };
+      break;
+    case ACTIONS.SCISSORS:
+      return { player: scissors, computer: window[`${action.payload}`] };
+      break;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [imageState, dispatch] = useReducer(reducer, {player: null, computer: null});
   const [playerScore, setPlayerScore] = useState(() => 0);
   const [compScore, setCompScore] = useState(() => 0);
-  const gameVals = ['rock', 'paper', 'scissors'];
-  const rock = <img src='./images/rps_rock.png' />;
-  const paper = <img src='./images/rps_paper.png' />;
-  const scisorrs = <img src='./images/rps_scissors.png' />;
-  let userInput, compInput, result;
+  const [userInput, setUserInput] = useState(() => '');
+  const [result, setResult] = useState(() => null);
+  let compInput;
 
   const playerWin = () => {
     setPlayerScore(currentScore => currentScore + 1);
@@ -21,10 +46,11 @@ function App() {
   }
 
   function btnOnClick(e) {
-    userInput = e.target.getAttribute('data-value');
+    setUserInput(currentVal => e.target.getAttribute('data-value'));
     compInput = computerPlay();
+    dispatch({ type: userInput, payload: compInput });
     console.log(`computerInput =\t${compInput}`);
-    result = playRound(userInput, compInput);
+    setResult(currentResult => playRound(userInput, compInput));
     switch (result) {
       case 'win':
         playerWin();
@@ -50,18 +76,23 @@ function App() {
             <h2>{playerScore}</h2>
           </div>
           <div className="player-img">
+            {paper}
           </div>
           <div className="computer-img">
+            {rock}
           </div>
           <div className="computer">
             <h1>Comp</h1>
             <h2>{compScore}</h2>
           </div>
         </div>
+        <div className="result">
+          <h2>{result}</h2>
+        </div>
         <div className="btn-group">
-          <button onClick={btnOnClick} data-value={gameVals[0]}>Rock</button>
-          <button onClick={btnOnClick} data-value={gameVals[1]}>Paper</button>
-          <button onClick={btnOnClick} data-value={gameVals[2]}>Scissors</button>
+          <button onClick={btnOnClick} data-value={ACTIONS.ROCK}>Rock</button>
+          <button onClick={btnOnClick} data-value={ACTIONS.PAPER}>Paper</button>
+          <button onClick={btnOnClick} data-value={ACTIONS.SCISSORS}>Scissors</button>
         </div>
       </div>
     </div>
